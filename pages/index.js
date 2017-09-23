8a3993c21fec69efd4e0aff30703b9dec0fd7751
Ben <benjamin.andremicolon@gmail.com>
2017-09-24 00:54:14 +0300

    Update website / add scss support / remove portfolio page

--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,30 @@
 //@flow
-import React from 'react';
-import Link from 'next/link'
-import Head from 'next/head';
+import React from "react";
+import Link from "next/link";
+import Head from "../components/Head";
 
 export default () => {
+   const handleChickEyeClick = () => {
+      const audio = new Audio("/static/horse-neighing.mp3");
+      return audio.play();
+   };
 
-  const handleChickEyeClick = () => {
-    const audio = new Audio('/static/horse-neighing.mp3');
-    return audio.play();
-  }
-
-  return (
-    <div>
-      <Head>
-        <title>bendersej</title>
-      </Head>
-      <div className="main">
-        <div className="chick">
-          <div className="chick__eye" onClick={handleChickEyeClick}/>
-        </div>
-        <div>
-          <h1>bendersej</h1>
-          <p>> about</p>
-        </div>
+   return (
+      <div>
+         <Head title="bendersej" />
+         <div className="home">
+            <div className="home__chick">
+               <div className="home__chick-eye" onClick={handleChickEyeClick} />
+            </div>
+            <div>
+               <h1>bendersej</h1>
+               <div className="home__header">
+                  <Link prefetch href="/about">
+                     <a className="home__header-link">about</a>
+                  </Link>
+               </div>
+            </div>
+         </div>
       </div>
-      <style jsx>{`
-			.main {
-        display: flex;
-        height: 100vh;
-        align-items: center;
-        flex-direction: column;
-        justify-content: center;
-      }
-
-      h1 {
-        color: black;
-        font-family: 'Inconsolata', monospace;
-        font-weight: 400;
-        font-size: 80px;
-        margin: 50px 0 0 0;
-        cursor: pointer;
-
-        &:hover {
-          color: red;
-        }
-      }
-
-      .chick {
-        position: relative;
-        width: 450px;
-        height: 450px;
-        background-image: url('/static/img/chick.svg');
-      }
-      
-      .chick__eye {
-        position: absolute;
-        cursor: pointer;
-        top: 90px;
-        left: 130px;
-        width: 18px;
-        height: 18px;
-        border-radius: 100%;
-        background: black;
-        animation: squeeze 2s infinite;
-      }
-
-      @keyframes squeeze {
-        90% {
-          transform: none;
-          animation-timing-function: ease-in;
-        }
-        93% {
-          transform: translateY(3px) scaleY(0.8)
-        }
-        100% {
-          animation-timing-function: ease-out;
-        }
-      }
-    `}</style>
-    </div>
-  );
-
-}
+   );
+};
