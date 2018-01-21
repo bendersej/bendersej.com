8ef486ac858b7801c658507e53d0c9bd43d0d8b4
Ben <benjamin.andremicolon@gmail.com>
2018-01-21 14:18:12 +0200

    Add touch-icon and noopener rel on external links (Lightroom Audit recommendations)

--- components/Tile.js
+++ components/Tile.js
@@ -1,24 +1,24 @@
 export default ({ content, isHoverable }) => (
-   <a
-      className={isHoverable ? "tile tile--hoverable" : "tile"}
-      href={content.href}
-      target="_blank"
-   >
-      <div className="tile__img-container">
-         <img src={content.img} />
-      </div>
+  <a
+    className={isHoverable ? 'tile tile--hoverable' : 'tile'}
+    href={content.href}
+    target="_blank"
+    rel="noopener">
+    <div className="tile__img-container">
+      <img src={content.img} />
+    </div>
 
-      <div className="tile__content">
-         <h4>{content.title}</h4>
-         <p>{content.duration}</p>
-         <div className="tile__tag-container">
-            {content.skills &&
-               content.skills.map((skill, idx) => (
-                  <span className="tile__tag" key={idx}>
-                     {skill}
-                  </span>
-               ))}
-         </div>
+    <div className="tile__content">
+      <h4>{content.title}</h4>
+      <p>{content.duration}</p>
+      <div className="tile__tag-container">
+        {content.skills &&
+          content.skills.map((skill, idx) => (
+            <span className="tile__tag" key={idx}>
+              {skill}
+            </span>
+          ))}
       </div>
-   </a>
+    </div>
+  </a>
 );
