c14d4f043e60c126ac9a06260e5bca652aa470c5
Ben <benjamin.andremicolon@gmail.com>
2020-01-04 17:13:39 +0200

    feat: add ga tracking

--- components/Head.js
+++ components/Head.js
@@ -1,118 +1,123 @@
 import Head from 'next/head';
+import { useGoogleAnalytics } from '../lib/react-ga';
 
-export default (props) => (
-  <React.Fragment>
-    <Head>
-      <title>{props.title || ''}</title>
-      <meta name="description" content={props.description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-      <link rel="icon" sizes="192x192" href="/touch-icon.png" />
-      <link rel="apple-touch-icon" href="/touch-icon.png" />
-      <link rel="mask-icon" href="/favicon-mask.svg" color="#49B882" />
-      <link rel="icon" href="/favicon.ico" />
-      <meta property="og:url" content={props.url} />
-      <meta property="og:title" content={props.title} />
-      <meta property="og:description" content={props.description} />
-      <meta name="twitter:site" content={props.url} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={props.ogImage} />
-      <meta property="og:image" content={props.ogImage} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet" />
-    </Head>
-    <style jsx global>{`
-      body {
-        font-family: 'Inconsolata', monospace;
-        font-size: 16px;
-        margin: 0;
-        max-width: 768px;
-        margin: 0 auto;
-        line-height: 1;
-      }
+export default (props) => {
+  useGoogleAnalytics();
 
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-      p {
-        margin: 0;
-      }
+  return (
+    <React.Fragment>
+      <Head>
+        <title>{props.title || ''}</title>
+        <meta name="description" content={props.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="icon" sizes="192x192" href="/touch-icon.png" />
+        <link rel="apple-touch-icon" href="/touch-icon.png" />
+        <link rel="mask-icon" href="/favicon-mask.svg" color="#49B882" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content={props.url} />
+        <meta property="og:title" content={props.title} />
+        <meta property="og:description" content={props.description} />
+        <meta name="twitter:site" content={props.url} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={props.ogImage} />
+        <meta property="og:image" content={props.ogImage} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet" />
+      </Head>
+      <style jsx global>{`
+        body {
+          font-family: 'Inconsolata', monospace;
+          font-size: 16px;
+          margin: 0;
+          max-width: 768px;
+          margin: 0 auto;
+          line-height: 1;
+        }
 
-      h1,
-      h2,
-      h3,
-      h4 {
-        margin: 0;
-        font-weight: 400;
-      }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+        p {
+          margin: 0;
+        }
 
-      circle,
-      path {
-        transition: 0.3s linear;
-      }
+        h1,
+        h2,
+        h3,
+        h4 {
+          margin: 0;
+          font-weight: 400;
+        }
 
-      .tooltip {
-        display: inherit;
-        position: relative;
+        circle,
+        path {
+          transition: 0.3s linear;
+        }
 
-        &:hover {
-          z-index: 1;
+        .tooltip {
+          display: inherit;
+          position: relative;
 
-          &::after,
-          &::before {
-            opacity: 1;
-            transition-delay: 0.2s;
+          &:hover {
+            z-index: 1;
+
+            &::after,
+            &::before {
+              opacity: 1;
+              transition-delay: 0.2s;
+            }
+          }
+
+          &:before {
+            width: 0;
+            height: 0;
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+
+            border-bottom: 5px solid black;
+            content: '';
+
+            position: absolute;
+            top: 130%;
+            left: 0;
+            right: 0;
+            margin: -5px auto 0 auto;
+
+            pointer-events: none;
+            opacity: 0;
+          }
+
+          &:after {
+            box-sizing: border-box;
+            background: black;
+            color: white;
+            padding: 5px;
+            border-radius: 3px;
+            content: attr(aria-label);
+            display: flex;
+
+            font-size: 14px;
+            line-height: 1.2;
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 130%;
+            margin: 0 auto;
+            min-width: 100px;
+            pointer-events: none;
+            opacity: 0;
           }
         }
 
-        &:before {
-          width: 0;
-          height: 0;
-          border-left: 5px solid transparent;
-          border-right: 5px solid transparent;
-
-          border-bottom: 5px solid black;
-          content: '';
-
-          position: absolute;
-          top: 130%;
-          left: 0;
-          right: 0;
-          margin: -5px auto 0 auto;
-
-          pointer-events: none;
-          opacity: 0;
+        @media print {
+          body {
+            margin-top: 20px;
+          }
         }
-
-        &:after {
-          box-sizing: border-box;
-          background: black;
-          color: white;
-          padding: 5px;
-          border-radius: 3px;
-          content: attr(aria-label);
-          display: flex;
-
-          font-size: 14px;
-          line-height: 1.2;
-          position: absolute;
-          left: 0;
-          right: 0;
-          top: 130%;
-          margin: 0 auto;
-          min-width: 100px;
-          pointer-events: none;
-          opacity: 0;
-        }
-      }
-
-      @media print {
-        body {
-          margin-top: 20px;
-        }
-      }
-    `}</style>
-  </React.Fragment>
-);
+      `}</style>
+    </React.Fragment>
+  );
+};
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    GOOGLE_ANALYTICS_TAG: 'G-PL0V63JQ06',
+  },
+};
